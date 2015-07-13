2a1847f1fffb672fa7ea5994adf009a128f9b6e1
Shimpei Otsubo <potsbo@gmail.com>
2015-07-14 02:39:23 +0900

    abcdvorak files are now separate

--- abcdvorak.cpp
+++ abcdvorak.cpp
@@ -1,7 +1,18 @@
-/* #include <string> */
-/* #include "abcdvorak.h" */
+#include <string>
+#include "abcdvorak.h"
 
-/* ABCDvorak::ABCDvorak(string saveData){ */
-/* TODO: load saveData */
-/* } */
+ABCDvorak::ABCDvorak(string saveData ){};
+void ABCDvorak::run(){
+	for(int i = currentPhase; i < endPahese; i++ ){
+		string index = to_string(i);
+		if(index.size() == 1){
+			index = '0' + index;
+		}
+		string objectiveFile = "abcd" + index;
+
+		Game abcdGame(dvorakTable,objectiveFile);
+		abcdGame.setSeq(true);
+		abcdGame.run();
+	}
+}
 
--- abcdvorak.h
+++ abcdvorak.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 #include "game.h"
 
@@ -7,20 +9,6 @@ class ABCDvorak{
 		int endPahese = 29;
 		string dvorakTable = "ascii";
 	public:
-		ABCDvorak(string saveData = ""){}
-
-		void run(){
-			for(int i = currentPhase; i < endPahese; i++ ){
-				string index = to_string(i);
-				if(index.size() == 1){
-					index = '0' + index;
-				}
-				string objectiveFile = "abcd" + index;
-
-				Game abcdGame(dvorakTable,objectiveFile);
-				abcdGame.setSeq(true);
-				abcdGame.run();
-			}
-		}
-
+		ABCDvorak(string saveData = "");
+		void run();
 };
