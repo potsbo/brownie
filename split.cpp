3a49e2d8a7c3c7d4f49e8efafadd5020cf5941a8
Shimpei Otsubo <potsbo@gmail.com>
2015-07-14 02:38:44 +0900

    split files are now separate

---
+++ split.cpp
@@ -0,0 +1,14 @@
+#include "split.h"
+
+std::vector<std::string> split(const std::string& input, char delimiter){
+	std::istringstream stream(input);
+
+	std::string field;
+	std::vector<std::string> result;
+	while (std::getline(stream, field, delimiter)) {
+		result.push_back(field);
+	}
+	return result;
+}
+
+
--- split.h
+++ split.h
@@ -4,14 +4,5 @@
 #include <string>
 #include <sstream>
 
-std::vector<std::string> split(const std::string& input, char delimiter){
-	std::istringstream stream(input);
-
-	std::string field;
-	std::vector<std::string> result;
-	while (std::getline(stream, field, delimiter)) {
-		result.push_back(field);
-	}
-	return result;
-}
+std::vector<std::string> split(const std::string& input, char delimiter);
 
