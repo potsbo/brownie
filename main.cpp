0117208ae07efaf30e901768b9caeec25142a226
Shimpei Otsubo <potsbo@gmail.com>
2015-07-14 04:27:00 +0900

    able to set objective file

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ bool debugFlag = false;
 int main(int argc, char *argv[]) {
 	cmdline::parser a;
 	a.add("debug", 'd', "debagFlag = true に設定します。");
-	a.add<string>("table", 't', "ローマ字テーブルファイル名", false, "./googleNihongoRomajiTable");
+	a.add<string>("table", 't', "ローマ字テーブルファイル名", false, "googleNihongoRomajiTable");
+	a.add<string>("objective", 'o', "文字列ファイル指定", false, "default");
 	a.add("abcd", '\0', "A Basic Course in Dvorak");
 	a.add("sequence", 's', "文字列を順に表示(デフォルトはランダム)");
 	a.parse_check(argc, argv);
@@ -21,12 +22,13 @@ int main(int argc, char *argv[]) {
 	cout << "Brownie ver. " << VERSION << endl;
 
 	string sourceFile = a.get<string>("table");
+	string objectiveFile = a.get<string>("objective");
 
 	if(a.exist("abcd")){
 		ABCDvorak newABCD;
 		newABCD.run();
 	}else{
-		Game newGame(sourceFile);
+		Game newGame(sourceFile,objectiveFile);
 		newGame.setSeq(a.exist("sequence"));
 		newGame.run();
 	}
