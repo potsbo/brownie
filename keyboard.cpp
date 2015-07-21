df908dbb1c0a4e11c89eb17e5ff341fc0390912c
Shimpei Otsubo <potsbo@gmail.com>
2015-07-22 00:09:41 +0900

    fixed merge conflict

--- keyboard.cpp
+++ keyboard.cpp
@@ -5,11 +5,5 @@ char getKeyboardInput(){
 	system ("/bin/stty raw -echo");
 	char inputChar = getchar();
 	system ("/bin/stty cooked echo");
-	if(inputChar <= 31){
-		switch(inputChar){
-			case 3:
-				exit(1);
-		}
-	}
 	return inputChar;
 }
