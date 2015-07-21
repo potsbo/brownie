2d8847be1366a276b3e97047374c692054f78d7e
Shimpei Otsubo <potsbo@gmail.com>
2015-07-22 04:36:29 +0900

    add basic functionality of typo analyze

--- typo.cpp
+++ typo.cpp
@@ -3,9 +3,29 @@ Typo::Typo(char c){
 	this->c = c;
 }
 
+void Typo::increment (){
+	this->cnt++;
+}
+
 void TypoSet::analyze(){
 	/* TODO: implement analyze() */
+	int alloverTypoCnt;
+	for(char c : typoList){
+		std::vector<Typo>::iterator itr;
+		bool found = false;
+		while(itr != typoSet.end()){
+			if((*itr).getChara() == c){
+				(*itr).increment();
+				found = true;
+				break;
+			}
+		}
+		if(!found){
+			typoSet.push_back(c);
+		}
+	}
 }
+
 void TypoSet::add(char c){
 	typoList.push_back(c);
 	tempCnt++;
--- typo.h
+++ typo.h
@@ -7,15 +7,18 @@ class Typo {
 		char c;
 	public:
 		Typo(char c);
+		int getCnt();
+		int getChara();
 		void increment();
 };
 
 class TypoSet  {
 	private:
-		std::set<Typo> typoSet;
+		std::vector<Typo> typoSet;
 		std::vector<char> typoList;
 		int tempCnt = 0;
 		int mistakeMax = 0;
+		int rankTopOf = 5;
 		void analyze();
 	public:
 		void add(char c);
